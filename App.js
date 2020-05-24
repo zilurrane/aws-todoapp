04fad01fa2cb7ce2c7cb199afcfde601b39655a6
root <root@pop-os.localdomain>
2020-05-24 23:45:18 +0530

    Add subscriptions to fetch real time updates

--- App.js
+++ App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Amplify from 'aws-amplify'
 import config from './aws-exports'
+import Todo from './Todo';
+import { withAuthenticator } from 'aws-amplify-react-native'
+
 Amplify.configure(config)
 
-export default function App() {
+export default withAuthenticator(function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>TodoMate</Text>
+      <Todo></Todo>
     </View>
   );
-}
+});
 
 const styles = StyleSheet.create({
   container: {
---
+++ Todo.js
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from 'react'
+import {
+  View, Text, StyleSheet, TextInput, Button
+} from 'react-native'
+
+import { API, graphqlOperation } from 'aws-amplify'
+import { createTodo } from './src/graphql/mutations'
+import { listTodos } from './src/graphql/queries'
+import { onCreateTodo } from './src/graphql/subscriptions'
+
+const initialState = { name: '', description: '' }
+
+const Todo = () => {
+  const [formState, setFormState] = useState(initialState)
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    fetchTodos(),
+    reloadTodo()
+  }, [])
+
+  function setInput(key, value) {
+    setFormState({ ...formState, [key]: value })
+  }
+
+  async function fetchTodos() {
+    try {
+      const todoData = await API.graphql(graphqlOperation(listTodos))
+      const todos = todoData.data.listTodos.items
+      setTodos(todos)
+    } catch (err) { console.log('error fetching todos') }
+  }
+
+  function reloadTodo() {
+    console.log('todos', todos);
+    const subscription = API.graphql(
+      graphqlOperation(onCreateTodo)
+    ).subscribe({
+      next: event => {
+        if (event){
+          console.log("Subscription: " + JSON.stringify(event.value.data, null, 2), event);
+          const todo = event.value.data.onCreateTodo;
+          console.log(todos, todo);
+          setTodos([...todos, todo])
+        }
+      }
+    });
+  }
+
+  async function addTodo() {
+    try {
+      const todo = { ...formState }
+      setTodos([...todos, todo])
+      setFormState(initialState)
+      await API.graphql(graphqlOperation(createTodo, { input: todo }))
+    } catch (err) {
+      console.log('error creating todo:', err)
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        onChangeText={val => setInput('name', val)}
+        style={styles.input}
+        value={formState.name}
+        placeholder="Name"
+      />
+      <TextInput
+        onChangeText={val => setInput('description', val)}
+        style={styles.input}
+        value={formState.description}
+        placeholder="Description"
+      />
+      <Button title="Create Todo" onPress={addTodo} />
+      {
+        todos.map((todo, index) => (
+          <View key={todo.id ? todo.id : index} style={styles.todo}>
+            <Text style={styles.todoName}>{todo.name}</Text>
+            <Text> - {todo.description}</Text>
+          </View>
+        ))
+      }
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', padding: 20 },
+  todo: { marginBottom: 15 },
+  input: { height: 50, backgroundColor: '#ddd', marginBottom: 10, padding: 8 },
+  todoName: { fontSize: 18, fontWeight: 900 }
+})
+
+export default Todo
