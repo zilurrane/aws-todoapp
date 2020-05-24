466e317004f804e96e7d4ca7d487e4cd9745df84
root <root@pop-os.localdomain>
2020-05-24 18:21:08 +0530

    Added amplify scaffold

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Amplify from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config)
 
 export default function App() {
   return (
